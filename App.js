aed55f4c4c0e0da1fe1f021b576cca556b3ca06f
Md Touhidul Islam <93336563+Asabenah@users.noreply.github.com>
2021-11-05 23:17:05 +0200

    Add files via upload
    Basic of react.js and it's components

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+import Info from "./Components/Info";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    
+     <Info/>
+    
+  );
+}
+
+export default App;
