7dcb166b0a5d0082fd272479772aa766481e3120
Md Touhidul Islam <93336563+Asabenah@users.noreply.github.com>
2021-11-06 01:00:14 +0200

    Add files via upload
    Jsx to javascript. How does it work.

--- App.js
+++ App.js
@@ -4,14 +4,33 @@ import React from "react";
 import Footer from "./Components/Footer"
 import Header from "./Components/Header"
 import MainContent from './Components/MainContent';
+import Checkbox from './Components/Checkbox';
 import ReactDOM from "react-dom";
 
 function App() {
+
+ const date = new Date()
+ const hours = date.getHours()
+ let timeOfDay
+
+ if (hours == 12) {
+   timeOfDay = "morning"
+ }
+
+ else if (hours >= 12 && hours <17){
+   timeOfDay = "afternoon"
+ }
+ else{
+   timeOfDay = "night"
+ }
+
   return (
     <div>
+      <h1>Good {timeOfDay } </h1>
       <Header/>
       <MainContent/>
      <Footer/>
+     <Checkbox/>
     </div>
      
     
